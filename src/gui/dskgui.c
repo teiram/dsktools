6e7e6e8890aa412f9894c2bbad098be817e167c8
Manuel Teira <mteira@indra.es>
2016-02-25 12:30:07 +0100

    Added a first design for the interface

--- src/gui/dskgui.c
+++ src/gui/dskgui.c
@@ -1,25 +1,24 @@
 #include <gtk/gtk.h>
 
 int main(int argc, char **argv) {
-	GtkBuilder *builder;
-	GtkWidget  *window;
 	GError     *error = NULL;
 
 	/* Init GTK+ */
-	gtk_init( &argc, &argv );
+	gtk_init(&argc, &argv);
 
 	/* Create new GtkBuilder object */
-	builder = gtk_builder_new();
+	GtkBuilder *builder = gtk_builder_new();
 	/* Load UI from file. If error occurs, report it and quit application.
 	 * Replace "tut.glade" with your saved project. */
-	if (!gtk_builder_add_from_file(builder, "tut.glade", &error)) {
+	if (!gtk_builder_add_from_file(builder, "interface.glade", &error)) {
 		g_warning("%s", error->message);
 		g_free(error);
 		return 1;
 	}
 
 	/* Get main window pointer from UI */
-	window = GTK_WIDGET(gtk_builder_get_object(builder, "window1"));
+	GtkWidget *window = 
+		GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
 
 	/* Connect signals */
 	gtk_builder_connect_signals(builder, NULL);
@@ -27,7 +26,8 @@ int main(int argc, char **argv) {
 	/* Destroy builder, since we don't need it anymore */
 	g_object_unref(G_OBJECT(builder));
 
-	/* Show window. All other widgets are automatically shown by GtkBuilder */
+	/* Show main window. Any other widget is automatically shown 
+	   by GtkBuilder */
 	gtk_widget_show(window);
 
 	/* Start main loop */
